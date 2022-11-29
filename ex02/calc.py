18ac77d4da1212192738580f323c7e649e1b4255
Yuto <c0a211134d@edu.teu.ac.jp>
2022-11-29 14:56:55 +0900

    第２回練習7

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,7 +5,10 @@ def button_click(event):
     btn = event.widget
     num = btn["text"]
     if num == "=":
-        pass
+        siki = entry.get()
+        res = eval(siki)
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, res)
     else:
     #tkm.showinfo("", f"{num}ボタンがクリックされました")
         entry.insert(tk.END, num)
@@ -16,7 +19,7 @@ root.geometry("300x500")
 entry = tk.Entry(root, justify="right", width=10, font=("", 40))
 entry.grid(row=0, column=0, columnspan=3)
 
-r, c = 0, 0
+r, c = 1, 0
 for num in range(9, -1, -1):
     button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
     button.grid(row=r, column=c)
@@ -30,6 +33,7 @@ operators = ["+", "="]
 for ope in operators:
     button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 30))
     button.grid(row=r, column=c)
+    button.bind("<1>", button_click)
     c += 1
     if c%3 == 0:
         r += 1
