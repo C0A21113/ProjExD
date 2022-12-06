6676f0c98b8f5b3dc85478bcc402f37241df7b61
Yuto <c0a211134d@edu.teu.ac.jp>
2022-12-06 14:29:45 +0900

    課題2-4

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,7 +1,15 @@
 import tkinter as tk
 
-
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
+    canvas = tk.Canvas(root, width=1500, height=900, bg="black")
+
+    yakitori = tk.PhotoImage(file="fig/8.png")
+    cx, cy = 300, 400
+    canvas.create_image(cx, cy, image=yakitori, tag="kokaton")
+    canvas.pack()
+
+    key = ""
+
     root.mainloop()
